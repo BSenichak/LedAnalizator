1093568afd05321123418599d315c720660f02b5
BSenichak <bsenk77@gmail.com>
2024-02-20 16:00:59 +0200

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            injectRegister: "auto",
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Led Analizator",
+                short_name: "LED Analiz",
+                start_url: "/?pwa=1",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#005caa",
+                lang: "uk-UA",
+                // shortcuts: [
+                //     {
+                //         name: "Створити заявку",
+                //         url: "/create",
+                //     },
+                // ],
+                icons: [
+                    {
+                        src: "/imageslogo.svg",
+                        type: "image/svg+xml",
+                        purpose: "any",
+                    }
+                ],
+            },
+        }),
+    ],
+    build: {
+      sourcemap: true
+    }
+});
