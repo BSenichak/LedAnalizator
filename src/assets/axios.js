ec4726ffbb3a43ef56ce10a1c99f990a84365eed
BSenichak <bsenk77@gmail.com>
2024-04-26 15:46:37 +0300

    add property to use in local

--- src/assets/axios.js
+++ src/assets/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: "http://192.168.31.212:3000/",
+
     // baseURL: "https://tfledanalysis.vercel.app",
 });
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+server: {
+    host: "0.0.0.0",
+}
+,
     plugins: [
         react(),
         VitePWA({
